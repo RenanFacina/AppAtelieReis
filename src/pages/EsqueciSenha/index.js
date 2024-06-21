8bc42a8e42dd4abbde0bc560b59f4f27ed9ab7e7
Kauan-Henrique-de-Souza <kauanhenrique5696@gmail.com>
2024-06-20 21:56:51 -0300

    update novo usuario e recupera senha

--- src/pages/EsqueciSenha/index.js
+++ src/pages/EsqueciSenha/index.js
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
+import { Pressable, View, Text, TextInput } from "react-native";
+import {styles} from "../Login/styles";
+import {useState} from "react";
+import { sendPasswordResetEmail } from "firebase/auth";
+import {auth} from "../../services/firebase.config";
 
-export function EsqueciSenha() {
-  return (
-    <View>
-      <Text>Tela Esqueci Senha</Text>
-    </View>
-  );
+export function EsqueciSenha({navigation}) {
+  const[userMail,setUserMail] = useState('');
+
+    function replacePass(){
+        if(userMail !== ''){ //Evitar conteudo vazio
+            sendPasswordResetEmail(auth,userMail)
+            .then(()=>{
+                alert("Foi enviado um email para: " +userMail+".\nVerifique a sua caixa de email.");
+                navigation.navigate('Login');
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                alert("Ops! Alguma coisa não deu certo. " + errorMessage + ". Tente novamente ou pressione voltar");
+                return;
+            });
+        } else{
+            alert("É preciso informar um e-mail válido para efetuar a redefinição de senha");
+            return;
+        }
+    }
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Redefinição de Senha</Text>
+            <TextInput style={styles.input}
+                placeholder="Informe o E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoComplete="email"
+                value={userMail}
+                onChangeText={setUserMail}
+            />
+            <Pressable style={styles.subBotao} onPress={replacePass}>
+                <Text style={styles.botao}>Enviar</Text>
+            </Pressable>
+            <View>
+               <Pressable onPress={()=> navigation.navigate("Login")}>
+                <Text style={styles.botao}>Voltar</Text>
+                </Pressable> 
+            </View>
+        </View>
+    );
 }
