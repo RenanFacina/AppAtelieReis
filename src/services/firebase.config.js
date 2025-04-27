ae8d4ff5c452976a0d79fc73932e470e7b28f133
Renan Facina <renan.ffacina@gmail.com>
2025-04-27 14:38:28 -0300

    Atualizado firebase.config.js

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -3,24 +3,9 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth"; //Aut
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 import { getDatabase } from "firebase/database"; //Banco de Dados Firebase
 
-//firebaseConfig Kauan
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAErrKyXourUIHD2bB-0J_W7YFg_nl45-g",
-//   authDomain: "login-ateliereis.firebaseapp.com",
-//   projectId: "login-ateliereis",
-//   storageBucket: "login-ateliereis.appspot.com",
-//   messagingSenderId: "6667615081",
-//   appId: "1:6667615081:web:326bda12ee5e7893fd9bf8",
-// };
-
-//firebaseConfig Renan
+//firebaseConfig
 const firebaseConfig = {
-  apiKey: "AIzaSyABRNWKDiN9sdUSC_m6Nc_LHPwOax8FL3E",
-  authDomain: "atelie-reis.firebaseapp.com",
-  projectId: "atelie-reis",
-  storageBucket: "atelie-reis.appspot.com",
-  messagingSenderId: "687704728966",
-  appId: "1:687704728966:web:a1214cc698e0c68fb77807",
+  //sua firebaseConfig aqui
 };
 
 // Initialize Firebase
